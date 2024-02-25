025eb3c82c19a25a531b5e4b9985d03eb1efa5e1
Fezi-x <tzayamin@gmail.com>
2024-02-25 14:47:55 +0630

    zmt home page card design

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
 'use client'
 import "./globals.css"
 import { useRouter } from "@/node_modules/next/navigation" 
+import { url } from "inspector"
 const IndexPage = ()=>{
     const router = useRouter()
 
     const ExploreBtn = () =>{
         router.push("/Country")
     }
-
+    const infoList = [{
+        title:'University',
+        description: 'Get the information about University/Collages around the world.',
+        image: '/image/uni_infoCard.png'
+    },{
+        title:'Country',
+        description: 'Include 20+ countries, just what you need to apply for.',
+        image:'/image/country_infoCard.png'
+    },{
+        title:'Visa',
+        description: 'Get the step by step details and knowledge about applying visa.',
+        image:'/image/visa_infoCard.png'
+    },{
+        title:'Passport',
+        description: 'Get passport information, what and how you can apply.',
+        image:'/image/pass_infoCard.png'
+    }] 
     return (
+        
         <div className="homePage">
             <div className="firsttDiv">
                 <div className="headTitle">
@@ -23,10 +41,20 @@ const IndexPage = ()=>{
                    <button className="button-77" role="button"  onClick={ExploreBtn}>Explore</button>                
                 </div>
             </div>
+           <div className = "containerSecondDiv">
             <div className="secondDiv">
+                {infoList.map((info)=>{
+                    return(
+                    <div className = "secDiv-1"  key={info.title} style={{ backgroundImage: `url(${info.image})` }}>
+                    <h2 className="secHeading">{info.title}</h2>
+                    <p className="pTag"><i>{info.description}</i></p>
+                    </div>
+                    )
+                })}
             </div>
+           </div>
+           
         </div>
-    )
+    )   
 }
-
 export default IndexPage
